e3fb91bfb81302aa01a7664ba4063e3b006570b5
Kazuki Hashimoto <eru.tndl@gmail.com>
2021-04-15 13:50:26 +0900

    :up: Using rison.

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import json
 from enum import Enum, auto
-from fastapi import FastAPI, Query, Body
-from pydantic import BaseModel, Field, Json
 from typing import Any, Optional
+
+import prison
+from fastapi import Body, FastAPI, HTTPException, Query, status
+from pydantic import BaseModel, Field, ValidationError
 
 app = FastAPI()
 
 
 class StrEnum(str, Enum):
     @staticmethod
-    def _generate_next_value_(name: str, start: int, count: int, last_values: list[Any]) -> str:
+    def _generate_next_value_(
+        name: str, start: int, count: int, last_values: list[Any]
+    ) -> str:
         return name.lower()
 
 
@@ -65,14 +68,18 @@ def options_filter(f: Filter, item: Item) -> bool:
 
 items = [
     Item(id=1, name="名前1", options={}),
-    Item(id=2, name="名前2", options={"int": 1, "float": 1.1, "date": "2021-01-01", "str": "foobazbar"}),
+    Item(
+        id=2,
+        name="名前2",
+        options={"int": 1, "float": 1.1, "date": "2021-01-01", "str": "foobazbar"},
+    ),
 ]
 
 
 @app.post("/items/search", response_model=list[Item])
 def search_items(
     name: str = Query("", title="name", description="nameの部分一致"),
-    filters: Filters = Body(Filters(), title="filter", description="options filter")
+    filters: Filters = Body(Filters(), title="filter", description="options filter"),
 ) -> Any:
     results = items
 
@@ -80,6 +87,37 @@ def search_items(
         results = list(filter(lambda x: name in x.name, results))
 
     for o in filters.options:
+        results = list(filter(lambda x: options_filter(o, x), results))
+
+    return results
+
+
+@app.get("/items", response_model=list[Item])
+def read_items(
+    name: str = Query("", title="name", description="nameの部分一致"),
+    filters: Optional[str] = Query(None, title="filter", description="options filter"),
+) -> Any:
+    if filters:
+        try:
+            decoded_filters = prison.loads(filters)
+            validated_filters = Filters(**decoded_filters)
+        except prison.decoder.ParserException as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=str(e),
+            )
+        except ValidationError as e:
+            raise HTTPException(
+                status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                detail=e.errors(),
+            )
+
+    results = items
+
+    if name:
+        results = list(filter(lambda x: name in x.name, results))
+
+    for o in validated_filters.options:
         results = list(filter(lambda x: options_filter(o, x), results))
 
     return results
